13d779f7d6c4e605070bb05021b35bbe4e40b2af
Alexander Obrezan <obrezan@gmail.com>
2024-10-05 19:57:24 +0300

    fix_some_issues

--- hw02_fix_app/main.go
+++ hw02_fix_app/main.go
@@ -1,24 +1,23 @@
-package init
+package main
 
 import (
+	"fmt"
+
 	"github.com/fixme_my_friend/hw02_fix_app/printer"
 	"github.com/fixme_my_friend/hw02_fix_app/reader"
 	"github.com/fixme_my_friend/hw02_fix_app/types"
-	"fmt"
 )
 
-func init() {
+func main() {
 	var path string = "data.json"
+	var err error
+	var staff []types.Employee
 
 	fmt.Printf("Enter data file path: ")
 	fmt.Scanln(&path)
 
-	var err error
-	var staff []types.Employee
-
 	if len(path) == 0 {
 		path = "data.json"
-	} else {
 	}
 
 	staff, err = reader.ReadJSON(path, -1)
